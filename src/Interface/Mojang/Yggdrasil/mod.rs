bb6367ce55270b07f4221924d11610bcb757c65c
Unili <mr.li0990@foxmail.com>
2022-09-02 09:16:55 +0800

    Initial Commit

--- src/Interface/Mojang/Yggdrasil/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod global;
-pub mod YggAuthenticate;
-pub mod YggInvalidate;
-pub mod YggRefresh;
-pub mod YggSignout;
-pub mod YggValidate;
--- src/Interface/Mojang/mod.rs
+++ src/Interface/Mojang/mod.rs
@@ -1,1 +1,10 @@
-pub mod Yggdrasil;
+pub mod global;
+
+// Yggdrasil APIs
+pub mod YggAuthenticate;
+pub mod YggInvalidate;
+pub mod YggRefresh;
+pub mod YggSignout;
+pub mod YggValidate;
+
+// Mojang APIs
